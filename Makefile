435715a214c1fd54ea69860dff85c80086a998b5
Marc Seiler <marcs@knowbe4.com>
2021-08-31 19:32:35 -0400

    version fun

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION := 0.1.3
+VERSION := $(shell cat VERSION)
 
 default: build
 
@@ -11,3 +11,7 @@ build:
 install: build
 	mkdir -p ~/.tflint.d/plugins/github.com/kb4sre/tflint-ruleset-kb4/$(VERSION)/
 	mv ./tflint-ruleset-kb4 ~/.tflint.d/plugins/github.com/kb4sre/tflint-ruleset-kb4/$(VERSION)/
+
+release:
+	git tag v$(VERSION)
+	git push origin v$(VERSION)
--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+
 	"github.com/kb4sre/tflint-ruleset-kb4/rules"
 	"github.com/terraform-linters/tflint-plugin-sdk/plugin"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
 func main() {
+
+	version, err := ioutil.ReadFile("VERSION") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "template",
-			Version: "0.1.3",
+			Version: string(version),
 			Rules: []tflint.Rule{
 				rules.NewTerraformValidatedVariablesRule(),
 			},
