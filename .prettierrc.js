594350d9f78eb57b2eb1d3b07191f15053a5fd09
Sharon Sheah <sharonsheah@gmail.com>
2022-06-20 17:02:24 +1000

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 120,
+  htmlWhitespaceSensitivity: 'strict',
+};
